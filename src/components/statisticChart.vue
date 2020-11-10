c38a92d41cce68cf4dcb861e52ebeffd18e88ef2
giser_yn@126.com <yn7224564049>
2020-11-10 15:30:05 +0800

    真统计图

--- src/components/statisticChart.vue
+++ src/components/statisticChart.vue
@@ -1,6 +1,5 @@
 <template>
   <v-container>
-
     <div ref="chart" style="width:100%;height: 300px"></div>
   </v-container>
 </template>
@@ -15,53 +14,36 @@ export default {
   },
   methods: {
     initCharts: function (myChart) {
-      // this.$axios.get(this.$URL.statisticData).then(
-      //     res => {
-      //         that.$message({
-      //           message: "删除成功",
-      //           type: 'success'
-      //         })
-      //     }
-      // )
-      let data = [{
-        name: '亚洲',
-        value: 70
-      }, {
-        name: '非洲',
-        value: 68
-      }, {
-        name: '欧洲',
-        value: 48
-      }, {
-        name: '北美洲',
-        value: 40
-      }, {
-        name: '大洋洲',
-        value: 32
-      }, {
-        name: '南美洲',
-        value: 27
-      }, {
-        name: '北极州',
-        value: 18
-      }];
-      let option = {
-        series: [{
-          type: 'pie',
-          radius: '25%',
-          center: ['50%', '50%'],
-          data: data,
-          animation: false,
-          label: {
-            position: 'outer',
-            alignTo: 'labelLine',
-            bleedMargin: 5
-          },
-          top: 0,
-          bottom: 0
-        }]
-      }
-      myChart.setOption(option)
+      this.$axios.get(this.$URL.statisticData).then(
+          res => {
+            let data = [];
+            for (let i = 0; i < res.body.length-1; i++) {
+              let datainfo={
+                name:res.body[i].country,
+                value:res.body[i].count
+              }
+              data.push(datainfo)
+            }
+            let option = {
+              series: [{
+                type: 'pie',
+                radius: '70%',
+                center: ['50%', '50%'],
+                data: data,
+                animation: false,
+                label: {
+                  position: 'outer',
+                  alignTo: 'labelLine',
+                  bleedMargin: 5
+                },
+                top: 0,
+                bottom: 0
+              }]
+            }
+            myChart.setOption(option)
+          }
+      )
+
     }
   }
 }

e70527b72fd38dedd171401304eafef5c582b4d1
giser_yn@126.com <yn7224564049>
2020-11-11 15:13:41 +0800

    manager 恢复功能

--- src/views/manager/data.vue
+++ src/views/manager/data.vue
@@ -261,7 +261,11 @@
                 let timelist=that.transferTime(timestamp)
 
                 console.log(timelist)
-                let logParams = {actId:this.$store.getters.username,role:this.$store.getters.role,time:timelist,tableName:that.typevalue,objectId:dataName,type:'删除'};
+                let logParams = {actId:this.$store.getters.username,
+                  role:this.$store.getters.role,
+                  time:timelist,
+                  tableName:that.typevalue,
+                  objectId:dataName,type:'删除'};
                 console.log(logParams)
                 that.$axios.put(that.$URL.addLog,logParams).then(
                     res => {
